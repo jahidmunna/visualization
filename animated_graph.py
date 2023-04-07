50e5c4092aeb6c3488a95b1be504e51bf66c9df1
Munna, Jahidulislam | Jahid | RMI <jahidulislam.munna@rakuten.com>
2023-04-07 09:48:22 +0900

    fixed typo

--- animated_graph.py
+++ animated_graph.py
@@ -59,5 +59,5 @@ class AnimatedHorizontalBarChart:
 
 if __name__ == '__main__':
     # Create an instance of the chart and animate it
-    chart = AnimatedHorizontalBarChart('gdf_sample.csv')
+    chart = AnimatedHorizontalBarChart('gdp_sample.csv')
     chart.animate_chart(save=True)
